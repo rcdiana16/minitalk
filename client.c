208530ffc4645fb93476d6144dc5b807559b0a50
rcdiana16 <carrillodiana17@gmail.com>
2025-02-16 12:58:56 +0100

    funciona (borre len s_h)

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: diana <diana@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:52:24 by diana             #+#    #+#             */
-/*   Updated: 2025/02/15 21:10:26 by diana            ###   ########.fr       */
+/*   Updated: 2025/02/16 12:28:31 by diana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,64 @@
 #include <stdio.h>
 #include <signal.h>
 
+void	send_signals(int pid, char *message)
+{
+	int				letter;
+	int				i;
+
+	letter = 0;
+	while (message[letter])
+	{
+		i = -1;
+		while (++i < 8)
+		{
+			if (((unsigned char)(message[letter] >> (7 - i)) & 1) == 0)
+				kill(pid, SIGUSR1);
+			else if (((unsigned char)(message[letter] >> (7 - i)) & 1) == 1)
+				kill(pid, SIGUSR2);
+			usleep(50);
+		}
+	letter++;
+	}
+	i = 0;
+	while (i++ < 8)
+	{
+		kill(pid, SIGUSR1);
+		usleep(50);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	char				*message;
+	int					server_id;
+
+	if (argc == 3)
+	{
+		server_id = ft_atoi(argv[1]);
+		if (!server_id)
+		{
+			ft_printf("[ERROR]. Wrong arg");
+			return (0);
+		}
+		message = argv[2];
+		if (message[0] == 0)
+		{
+			ft_printf("Tu n'as envoyé aucun texte ! Ecris qqch pls :)");
+			return (0);
+		}
+		send_signals(server_id, message);
+	}
+	else
+	{
+		ft_printf("[ERROR]. Too much or too few arguments.\n Make sure ");
+		ft_printf("you enter arguments as follow: ./client <PID> <MESSAGE>");
+	}
+	return (0);
+}
+
+
+/*
 void send_bit(int pid, int bit) {
     if (bit == 0) {
         kill(pid, SIGUSR1);  // Enviar SIGUSR1 (bit 0)
@@ -51,3 +109,4 @@ int main(int argc, char *argv[]) {
 	send_char(server_pid, '\0');
     return 0;
 }
+*/
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: diana <diana@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:52:28 by diana             #+#    #+#             */
-/*   Updated: 2025/02/15 21:10:36 by diana            ###   ########.fr       */
+/*   Updated: 2025/02/16 12:54:55 by diana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,81 @@
 #include <stdio.h>
 #include <signal.h>
 
+int	ft_recursive_power(int nb, int power)
+{
+	int	res;
+
+	if (power == 0)
+		return (1);
+	else if (power < 0)
+		return (0);
+	else
+	{
+		res = nb * ft_recursive_power(nb, power - 1);
+		return (res);
+	}
+}
+
+char	*letter_to_string(char const *s1, char const letter)
+{
+	int		i;
+	int		j;
+	char	*tab;
+
+	i = 0;
+	j = 0;
+	tab = malloc((ft_strlen(s1) + 2) * sizeof(char));
+	if (!tab)
+		return (NULL);
+	while (s1[i])
+		tab[j++] = s1[i++];
+	i = 0;
+	tab[j++] = letter;
+	tab[j] = 0;
+	free ((void *)(s1));
+	return (tab);
+}
+
+void	signal_handler(int signum)
+{
+	static int	counter = 0;
+	static int	result = 0;
+	static char	*final;
+
+	if (!final)
+		final = ft_strdup("");
+	if (signum == SIGUSR1)
+		result = result + 0;
+	else if (signum == SIGUSR2)
+		result = result + (1 * ft_recursive_power(2, 7 - counter));
+	counter++;
+	if (counter == 8)
+	{
+		final = letter_to_string(final, result);
+		if (result == '\0')
+		{
+			ft_printf("%s\n", final);
+			final = NULL;
+		}
+		counter = 0;
+		result = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	signal_received;
+
+	ft_printf("Server's PID: %d\n", getpid());
+	signal_received.sa_handler = signal_handler;
+	signal_received.sa_flags = 0;
+	sigaction(SIGUSR1, &signal_received, NULL);
+	sigaction(SIGUSR2, &signal_received, NULL);
+	while (1)
+		usleep(50);
+}
+
+/*
 void signal_handler(int sig) {
 	static int bit_count = 0;
     static unsigned char received_data = 0;
@@ -54,3 +129,4 @@ int main() {
 
     return 0;
 }
+*/
